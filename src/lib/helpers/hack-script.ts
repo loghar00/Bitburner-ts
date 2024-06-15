e9760a186d4b2ce263efbb2321429a18729255a4
Logan <loghar00@gmail.com>
2024-06-15 16:25:37 -0500

    added helper hack script
    - Purpose: helper script to hack target -> requires deploy script
    	-- grabs target from args passed from deploy script
    	-- loops hack infinitely
    - Issues:
    	- no control for hack, is as greedy as possible
    	- no logic to determine how many threads is best for
    	server

--- src/lib/helpers/hack-script.ts
+++ src/lib/helpers/hack-script.ts
@@ -1,6 +1,11 @@
 import { NS } from "@ns";
-import { calcBestTarget } from "lib/helpers/choose-best-server";
 
+// hack helper to be used with deploy-script
+// takes a target as an arg
 export async function main(ns: NS): Promise<void> {
-
+    // assumes target is already nuked and no ports to open
+    const hackTarget = ns.args[0].toString()
+    while(true){
+        await ns.hack(hackTarget)
+    }
 }
